996ef253a60a5aed5d5361ec2e8d86e080c59fdd
Gus Diaz <143527520+EnTune926@users.noreply.github.com>
2025-09-06 11:41:41 +0200

    Update OledTurboLight.h

--- app/src/OledTurboLight.h
+++ app/src/OledTurboLight.h
@@ -1,22 +1,30 @@
+#ifndef OLED_TURBO_LIGHT_H
+#define OLED_TURBO_LIGHT_H
+
 #include <Arduino.h>
 
 #define I2CPORT PORTD
-// A bit set to 1 in the DDR is an output, 0 is an INPUT
 #define I2CDDR DDRD
-// setting a port instruction takes 1 clock cycle
 #define I2C_CLK_LOW() I2CPORT = bOld
+
+#define MAX_CACHE 1024
+
+// I²C low-level helpers
 static inline void i2cByteOut(uint8_t b);
 void i2cBegin(uint8_t addr);
 void i2cWrite(uint8_t *pData, uint8_t bLen);
 void i2cEnd();
 static void I2CWrite(int iAddr, unsigned char *pData, int iLen);
+
+// OLED API
 void oledInit(uint8_t bAddr, uint8_t pre_charge_period, uint8_t refresh_frequency);
 void oledShutdown();
 void oledTurnOn();
-static void oledWriteCommand(unsigned char c);
 void oledSetContrast(unsigned char ucContrast);
-static void oledSetPosition(int x, int y);
-static void oledWriteDataBlock(unsigned char *ucBuf, int iLen);
+
+// Pixel & framebuffer
 int oledSetPixel(int x, int y, unsigned char ucColor);
-void oledLoadBMPPart(uint8_t *pBMP, int bytes, int offset);
+void oledLoadBMPPart(uint8_t *pBMP, int bytes = 1024, int offset = 0);
 void oledFill(unsigned char ucData);
+
+#endif
